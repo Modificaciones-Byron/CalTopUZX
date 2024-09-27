7de394e36b29593597f99290ba1fc330a15d1580
Modificaciones-Byron <128570925+Modificaciones-Byron@users.noreply.github.com>
2024-09-27 03:24:34 -1200

    Add files via upload

---
+++ software.js
@@ -0,0 +1,342 @@
+const logosPath = {
+    "AS PC": "logos/AS.png",
+    "AS": "logos/AS.png",
+    "ALYANZ GIRLS": "logos/ALYANZ.png",
+    "ALYANZ UZK": "logos/ALYANZ.png",
+    "ALYANZ E-SPORTS": "logos/ALYANZ.png",
+    "AS NUVS": "logos/AS.png",
+    "AS MOBILE": "logos/AS.png",
+    "ASG MOB": "logos/ASG.png",
+    "ASGARD": "logos/ASG.png",
+    "ASG PC": "logos/ASG.png",
+    "ASG PROSHU": "logos/ASG.png",
+    "AGS EU": "logos/AGS.png",
+    "AGSV2 ESPORTS": "logos/AGS.png",
+    "ALQ": "logos/ALQ.png",
+    "ALQ STORM": "logos/ALQ.png",
+    "ALQ PREMIER": "logos/ALQ.png",
+    "ALQ V2": "logos/ALQ.png",
+    "BLOODLUST": "logos/BLOODLUST.png",
+    "CLAN 666": "logos/CLAN_666.png",
+    "CABRITAS": "logos/CABRITAS.png",
+    "CLS": "logos/CLS.png",
+    "CLS UNNAMED": "logos/CLS.png",
+    "CLS UNNAMED R": "logos/CLS.png",
+    "CLS PITUDOS": "logos/CLS.png",
+    "CLS ROMPEKKS": "logos/CLS.png",
+    "CLS SIX9": "logos/CLS.png",
+    "CLS BLACK": "logos/CLS.png",
+    "CLS BLACKBOX": "logos/CLS.png",
+    "CLS ROMA": "logos/CLS.png",
+    "CLS CUYS": "logos/CLS.png",
+    "CLS MIX": "logos/CLS.png",
+    "DYNASTY": "logos/DYNASTY.png",
+    "DARK MYTH": "logos/DM.png",
+    "EXY TOP": "logos/EXY_TOP.png",
+    "EXY SA": "logos/EXY_SA.png",
+    "EXY BIN PEN": "logos/EXY_SA.png",
+    "EXY MOB": "logos/EXY_MOBILE.png",
+    "EXY MOBILE": "logos/EXY_MOBILE.png",
+    "EXY CREW": "logos/EXY_CREW.png",
+    "EXY PRUEBAS": "logos/EXY_CREW.png",
+    "EX WNG": "logos/EXWNG.png",
+    "FXL": "logos/NO.png",  
+    "GK PC": "logos/GK.png",
+    "GK 1ST": "logos/GK.png",
+    "GK ELITE PC": "logos/GK.png",
+    "GK MOBILE": "logos/GK.png",
+    "GLIZZY GLOBANT": "logos/GLIZZY.png",
+    "GLIZZY GANG": "logos/GLIZZY.png",
+    "GZ NIGHT": "logos/GLIZZY.png",
+    "GZ ELITE": "logos/GLIZZY.png",
+    "GZ XZ": "logos/GLIZZY.png",
+    "GZ MIDNIGHT": "logos/GLIZZY.png",
+    "7Z GLIZZY": "logos/GLIZZY.png",
+    "IGZ": "logos/IGZ.png",
+    "IGZ PC": "logos/IGZ.png",
+    "IGZ BLOODIE": "logos/IGZ.png",
+    "IGZ MOBILE": "logos/IGZ.png",
+    "IGZ SNIPER": "logos/IGZ.png",
+    "IGZ ELITE": "logos/IGZ.png",
+    "IGZ INFAMOUS": "logos/IGZ.png",
+    "RS ELITE": "logos/IGZ.png",
+    "INTERPOL": "logos/ITP.png",
+    "NOT": "logos/NOT.png",
+    "NWA": "logos/NWA.png",
+    "NEW DRAGON": "logos/NEWDRAGON.png",
+    "NEW MOB": "logos/NEWDRAGON.png",
+    "NOHAT": "logos/NOHAT.png",
+    "NWG": "logos/NWG.png",
+    "NWG PC": "logos/NWG.png",
+    "NWG MOBILE": "logos/NWG.png",
+    "NWG ELITE": "logos/NWG.png",
+    "NWG KING": "logos/NWG.png",
+    "PXS SPORTS": "logos/PXS.png",
+    "R3": "logos/R3.png",
+    "RKN": "logos/RKN.png",
+    "R6": "logos/RKN.png",  
+    "RAPTORS T": "logos/RT.png",
+    "RAPTORS TEAM": "logos/RT.png",
+    "RS ELITE": "logos/RS.png",
+    "RS": "logos/RS.png",
+    "REAL SMOKE": "logos/RS.png",
+    "PKB": "logos/PKB.png",
+    "PKB F1": "logos/PKB.png",
+    "SINEIRA": "logos/SN.png",
+    "LEVIATANES": "logos/LEV.png",
+    "T1": "logos/T1.png",
+    "T1 PC": "logos/T1.png",
+    "TTV": "logos/TTV.png",
+    "TEAM 21": "logos/TEAM21.png",
+    "UNNAMED": "logos/UNNAMED.png",
+    "UNNAMED R": "logos/UNNAMED.png",
+    "6XZ E-SPORTS": "logos/6XZ.png",
+    "UZX E-SPORTS": "logos/UZX_E-SPORT.png",
+    "UZX TEAM": "logos/UZX_TEAM.png",
+    "VIXENS FURY": "logos/VX.png",
+    "VIP E-SPORTS": "logos/VIP.png",
+    "VYPER TEAM": "logos/VYPER.png",
+    "WZE TEAM": "logos/WZE.png",
+    "WZE": "logos/WZE.png",
+    "O22": "logos/O22.png",
+    "ZENIN": "logos/ZENIN.png",
+    "21S": "logos/21S.png",
+        // Agrega más equipos y rutas de logos aquí
+};
+
+const positions = {
+    logo: [
+        [605, 915], [605, 1160], [605, 1400], [605, 1640],
+        [605, 1880], [605, 2120], [605, 2360], [605, 2600],
+    ],
+    equipo: [
+        [1060, 1099], [1060, 1335], [1060, 1578], [1060, 1815],
+        [1060, 2059], [1060, 2299], [1060, 2539], [1060, 2782],
+    ],
+    kills: [
+        [2115, 1110], [2115, 1347], [2115, 1585], [2115, 1826],
+        [2115, 2065], [2115, 2305], [2115, 2547], [2115, 2786],
+    ],
+    top: [
+        [2571, 1110], [2571, 1347], [2571, 1585], [2571, 1826],
+        [2571, 2065], [2571, 2305], [2571, 2547], [2571, 2786],
+    ],
+    total: [
+        [3020, 1110], [3020, 1347], [3020, 1585], [3020, 1826],
+        [3020, 2065], [3020, 2305], [3020, 2547], [3020, 2786],
+    ]
+};
+
+class Equipo {
+    constructor(nombre) {
+        this.nombre = nombre;
+        this.kills = Array(4).fill(0);
+        this.top = Array(4).fill(0);
+        this.totalKills = 0;
+        this.totalTop = 0;
+        this.totalPuntaje = 0;
+    }
+
+    calcularTotales() {
+        this.totalKills = this.kills.reduce((a, b) => a + b, 0);
+        this.totalTop = this.top.reduce((a, b) => a + this.calcularPuntosTop(b), 0);
+        this.totalPuntaje = this.totalKills + this.totalTop;
+    }
+
+    calcularPuntosTop(posicion) {
+        const puntos = {
+            1: 35, 2: 30, 3: 25, 4: 20,
+            5: 10, 6: 7, 7: 5, 8: 3,
+            9: 2, 10: 1
+        };
+        return puntos[posicion] || 0;
+    }
+}
+
+let equipos = [];
+let numEquipos = 0;
+const abecedario = "ABCDEFGHIJKLMNO"; // Letras del abecedario
+function iniciar() {
+    numEquipos = parseInt(document.getElementById("numEquipos").value);
+    if (numEquipos >= 1 && numEquipos <= 15) {
+        const form = document.getElementById("equiposForm");
+        form.innerHTML = "";  // Limpiar entradas anteriores
+        for (let i = 0; i < numEquipos; i++) {
+            const equipoDiv = document.createElement("div");
+            equipoDiv.className = "equipo";
+
+            // Crear etiqueta con la letra correspondiente
+            const letraEquipo = abecedario[i]; // Obtener letra del abecedario
+            const nombreLabel = document.createElement("label");
+            nombreLabel.textContent = `Equipo ${i + 1} - ${letraEquipo}:`;
+            equipoDiv.appendChild(nombreLabel);
+
+            const nombreInput = document.createElement("input");
+            nombreInput.placeholder = `Nombre del equipo ${letraEquipo}`;
+            equipoDiv.appendChild(nombreInput);
+
+            for (let j = 0; j < 4; j++) {
+                const killsInput = document.createElement("input");
+                killsInput.placeholder = `Sala ${j + 1} Kills:`;
+                killsInput.type = "number";
+                killsInput.min = "0";
+                equipoDiv.appendChild(killsInput);
+
+                const topInput = document.createElement("input");
+                topInput.placeholder = `Sala ${j + 1} Top (1-10):`;
+                topInput.type = "number";
+                topInput.min = "1";
+                topInput.max = "15";
+                equipoDiv.appendChild(topInput);
+            }
+            form.appendChild(equipoDiv);
+        }
+        document.getElementById("resultadosButton").disabled = false;
+    } else {
+        alert("El número de equipos debe estar entre 1 y 15.");
+    }
+}
+
+function generarResultados() {
+    equipos.length = 0;  // Reiniciar el array de equipos
+    const inputs = document.querySelectorAll(".equipo");
+    inputs.forEach(input => {
+        // Cambia esta línea para obtener el valor del nombre correctamente
+        const nombre = input.querySelector("input[placeholder^='Nombre del equipo']").value;
+        
+        const equipo = new Equipo(nombre);
+        for (let j = 0; j < 4; j++) {
+            equipo.kills[j] = parseInt(input.children[2 * j + 1].value) || 0;
+            equipo.top[j] = parseInt(input.children[2 * j + 2].value) || 0;
+        }
+        equipo.calcularTotales();
+        equipos.push(equipo);
+    });
+
+    equipos.sort((a, b) => b.totalPuntaje - a.totalPuntaje);
+    equipos.splice(8);  // Limitar a los 8 mejores equipos
+
+    // Generar resultados en archivo de texto
+    const resultsText = equipos.map(e => `${e.nombre},${e.totalKills},${e.totalTop},${e.totalPuntaje}`).join("\n");
+
+    // Aquí guardamos el archivo en la memoria (internamente)
+    localStorage.setItem('resultados', resultsText);
+
+    // Desbloquear el botón para generar la imagen
+    document.getElementById("imagenButton").disabled = false;
+}
+
+
+function crearImagen() {
+    const canvas = document.createElement("canvas");
+    const ctx = canvas.getContext("2d");
+    const img = new Image();
+    img.src = "Plantilla/Tabla.png";  // Ruta a la imagen de la plantilla
+    img.onload = function () {
+        canvas.width = img.width;
+        canvas.height = img.height;
+        ctx.drawImage(img, 0, 0);
+
+        let logosCargados = 0;
+
+        equipos.forEach((equipo, index) => {
+            const logo = new Image();
+            logo.src = logosPath[equipo.nombre] || "logos/default.png"; // logo por defecto si no se encuentra
+
+            logo.onload = function () {
+                ctx.drawImage(logo, ...positions.logo[index], 260, 260);  // Dibujar logo
+
+                // Cambiar color para el primer equipo
+                if (index === 0) {
+                    ctx.fillStyle = "yellow";  // Nombre en amarillo para el primer equipo
+                } else {
+                    ctx.fillStyle = "white";  // Nombre en blanco para los demás
+                }
+
+                // Establecer la fuente y el tamaño correctos para los nombres de equipos
+                ctx.font = "95px impact";
+
+                // Dibujar nombres de equipos (alineación izquierda)
+                ctx.textAlign = "left";
+                ctx.fillText(equipo.nombre, ...positions.equipo[index]);
+
+                // Cambiar color de kills, top y total para el primer equipo
+                if (index === 0) {
+                    ctx.fillStyle = "black";  // Kills, top y total en negro para el primer equipo
+                } else {
+                    ctx.fillStyle = "white";  // Kills, top y total en blanco para los demás equipos
+                }
+
+                // Establecer la fuente y el tamaño correctos para los números (kills, tops, y total)
+                ctx.font = "125px impact";  // Tamaño de fuente para kills, top y total
+                ctx.textAlign = "center";   // Centramos el texto horizontalmente
+
+                // Dibujar kills centrados
+                ctx.fillText(equipo.totalKills, positions.kills[index][0], positions.kills[index][1]);
+
+                // Dibujar top centrados
+                ctx.fillText(equipo.totalTop, positions.top[index][0], positions.top[index][1]);
+
+                // Dibujar total puntaje centrado
+                ctx.fillText(equipo.totalPuntaje, positions.total[index][0], positions.total[index][1]);
+
+                // Verificar si ya se han cargado todos los logos
+                logosCargados++;
+                if (logosCargados === equipos.length) {
+                    setTimeout(() => {
+                        const imgURL = canvas.toDataURL("image/png");
+                        const link = document.createElement("a");
+                        link.href = imgURL;
+                        link.download = "Tabla_Resultados.png";
+                        link.click();
+                    }, 1000);  // Asegurarse de que todos los elementos se hayan dibujado
+                }
+            };
+
+            // Si hay un error cargando el logo, dibuja solo el texto
+            logo.onerror = function () {
+                if (index === 0) {
+                    ctx.fillStyle = "yellow";  // Nombre en amarillo para el primer equipo
+                } else {
+                    ctx.fillStyle = "white";  // Nombre en blanco para los demás
+                }
+
+                // Dibujar nombres de equipos
+                ctx.font = "95px impact";
+                ctx.textAlign = "left";
+                ctx.fillText(equipo.nombre, ...positions.equipo[index]);
+
+                // Cambiar color de kills, top y total para el primer equipo
+                if (index === 0) {
+                    ctx.fillStyle = "black";  // Kills, top y total en negro para el primer equipo
+                } else {
+                    ctx.fillStyle = "white";  // Kills, top y total en blanco para los demás
+                }
+
+                // Dibujar kills, top y total centrados con el tamaño adecuado
+                ctx.font = "125px impact";  // Tamaño correcto para los números
+                ctx.textAlign = "center";
+
+                ctx.fillText(equipo.totalKills, positions.kills[index][0], positions.kills[index][1]);
+                ctx.fillText(equipo.totalTop, positions.top[index][0], positions.top[index][1]);
+                ctx.fillText(equipo.totalPuntaje, positions.total[index][0], positions.total[index][1]);
+
+                logosCargados++;
+                if (logosCargados === equipos.length) {
+                    setTimeout(() => {
+                        const imgURL = canvas.toDataURL("image/png");
+                        const link = document.createElement("a");
+                        link.href = imgURL;
+                        link.download = "Tabla_Resultados.png";
+                        link.click();
+                    }, 1000);
+                }
+            };
+        });
+    };
+}
+
+
+document.getElementById("iniciarButton").onclick = iniciar;
+document.getElementById("resultadosButton").onclick = generarResultados;
+document.getElementById("imagenButton").onclick = crearImagen;
